62b8865b8477112f9afb9fbff832495b9ff52cbe
StevenEgnaczyk <stevenegnaczyk@gmail.com>
2020-06-12 18:32:27 -0400

    Rebased sorting algorthms into ones I can actually visualize using arrayLists. Added a few, need to rebase the functions in the actual class now.

--- main/src/Visualizer.java
+++ main/src/Visualizer.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import javax.tools.Tool;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -79,7 +81,7 @@ public class Visualizer extends  JFrame {
                     break;
                 case 1:
                     int sortingAlgorithmSelection = getSortingAlgorithmChoice();
-                    runSortingAlgorithm(sortingAlgorithmSelection, pBoard.pList, g2d, delay);
+                    runSortingAlgorithm(sortingAlgorithmSelection, pBoard.pList, g2d, delay, size);
                     break;
                 case 2:
                     delay = getNewDelay(delay);
@@ -110,13 +112,14 @@ public class Visualizer extends  JFrame {
     public int getSortingAlgorithmChoice() {
         System.out.println("Which Sorting Algorithm Would you like to use?");
         System.out.println("----------------------------------------------");
-        System.out.println("(0) - Selection Sort || (7) - Bucket Sort");
-        System.out.println("(1) - Insertion Sort || (8) - Shell Sort");
-        System.out.println("(2) - Bubble Sort    || (9) - Cocktail Sort");
-        System.out.println("(3) - Merge Sort     || (10) - Counting Sort");
-        System.out.println("(4) - Quick Sort     || (11) - Cube Sort");
-        System.out.println("(5) - Heap Sort      || (12) - Tree Sort");
-        System.out.println("(6) - Radix Sort     || (13) - Tim Sort");
+        System.out.println("(0) - Selection Sort || (8) - Tim Sort");
+        System.out.println("(1) - Insertion Sort || (9) - Bucket Sort");
+        System.out.println("(2) - Gnome Sort     || (10) - Quick Sort");
+        System.out.println("(3) - Shell Sort     || (11) - Bitonic Sort");
+        System.out.println("(4) - Bubble Sort    || (12) - Tree Sort");
+        System.out.println("(5) - Cocktail Sort  || (13) - Heap Sort");
+        System.out.println("(6) - Comb Sort      || (14) - Smooth Sort");
+        System.out.println("(7) - Merge Sort     || (15) - Pidgeonhole Sort");
 
         Scanner userChoice = new Scanner(System.in);
         return userChoice.nextInt();
@@ -134,7 +137,7 @@ public class Visualizer extends  JFrame {
         return userChoice.nextInt();
     }
 
-    public void runSortingAlgorithm(int sortingAlgorithmChoice, ArrayList<pixelColor> pcArrayList, Graphics g2d, int delay) {
+    public void runSortingAlgorithm(int sortingAlgorithmChoice, ArrayList<pixelColor> pcArrayList, Graphics g2d, int delay, int pixelWidth) {
         switch (sortingAlgorithmChoice) {
             case 0:
                 System.out.println("Running Selection Sort");
@@ -147,60 +150,71 @@ public class Visualizer extends  JFrame {
                 System.out.println("List sorted using Insertion Sort");
                 break;
             case 2:
-                System.out.println("Running Bubble Sort");
-                bubbleSort(pcArrayList,g2d,delay);
-                System.out.println("List sorted using Bubble Sort");
+                System.out.println("Running Gnome Sort");
+                gnomeSort(pcArrayList,g2d,delay);
+                System.out.println("List sorted using Gnome Sort");
                 break;
             case 3:
-                System.out.println("Running Merge Sort");
-                mergeSort(pcArrayList,0,pcArrayList.size()-1,g2d,delay);
-                System.out.println("List sorted using Bubble Sort");
-                break;
-            case 4:
-                System.out.println("Running Quick Sort");
-                quickSort(pcArrayList,0,pcArrayList.size()-1,g2d,delay);
-                System.out.println("List sorted using Quick Sort");
-                break;
-            case 5:
-                System.out.println("Running Heap Sort");
-                //HeapSort
-                System.out.println("List sorted using Quick Sort");
-                break;
-            case 6:
-                System.out.println("Running Radix Sort");
-                //Radix Sort
-                System.out.println("List sorted using Radix Sort");
-                break;
-            case 7:
-                System.out.println("Running Bucket Sort");
-                bucketSort(pcArrayList, pcArrayList.size()/10, g2d, delay);
-                System.out.println("List sorted using Bucket Sort");
-                break;
-            case 8:
                 System.out.println("Running Shell Sort");
                 shellSort(pcArrayList,g2d,delay);
                 System.out.println("List Sorted using Sort");
                 break;
-            case 9:
+            case 4:
+                System.out.println("Running Bubble Sort");
+                bubbleSort(pcArrayList,g2d,delay);
+                System.out.println("List sorted using Bubble Sort");
+                break;
+            case 5:
                 System.out.println("Running Cocktail Sort");
                 cocktailSort(pcArrayList,g2d,delay);
                 System.out.println("List Sorted using Cocktail Sort");
                 break;
-            case 10:
-                System.out.println("Running Count Sort");
-                countingSort(pcArrayList,g2d,delay);
+            case 6:
+                System.out.println("Running Comb Sort");
+                //radixSort(pcArrayList, pcArrayList.size(), g2d, delay);
+                System.out.println("List sorted using Comb Sort");
                 break;
+            case 7:
+                System.out.println("Running Merge Sort");
+                mergeSort(pcArrayList,0,pcArrayList.size()-1,g2d,delay);
+                System.out.println("List sorted using Bubble Sort");;
+                break;
+            case 8:
+                System.out.println("Running Tim Sort");
+                //bucketSort(pcArrayList, pcArrayList.size()/10, g2d, delay, pixelWidth);
+                System.out.println("List sorted using Tim Sort");
+                break;
+            case 9:
+                System.out.println("Running Bucket Sort");
+                bucketSort(pcArrayList, pcArrayList.size()/10, g2d, delay, pixelWidth);
+                System.out.println("List sorted using Bucket Sort");
+                break;
+            case 10:
+                System.out.println("Running Quick Sort");
+                quickSort(pcArrayList,0,pcArrayList.size()-1,g2d,delay);
+                System.out.println("List sorted using Quick Sort");
             case 11:
-                System.out.println("Running Selection Sort");
-                //Cube Sort
+                System.out.println("Running Bitonic Sort");
+                //quickSort(pcArrayList,0,pcArrayList.size()-1,g2d,delay);
+                System.out.println("List sorted using Bitonic Sort");
                 break;
             case 12:
-                System.out.println("Running Selection Sort");
-                //Tree Sort
+                System.out.println("Running Tree Sort");
+                //heapSort(pcArrayList, g2d, delay);
+                System.out.println("List sorted using Tree Sort");
                 break;
             case 13:
-                System.out.println("Running Selection Sort");
-                //Tim Sort
+                System.out.println("Running Heap Sort");
+                heapSort(pcArrayList, g2d, delay);
+                System.out.println("List sorted using Heap Sort");
+            case 14:
+                System.out.println("Running Smooth Sort");
+                //heapSort(pcArrayList, g2d, delay);
+                System.out.println("List sorted using Smooth Sort");
+            case 15:
+                System.out.println("Running Pidgeonhole Sort");
+                heapSort(pcArrayList, g2d, delay);
+                System.out.println("List sorted using Pidgeonhole Sort");
             default:
                 System.out.println("Not a Valid Argument. Try Again");
         }
@@ -259,7 +273,7 @@ public class Visualizer extends  JFrame {
             for (int j = i + 1; j < n; j++) {
                 jLocation = pList.get(j).getIndex();
                 if (jLocation < minLocation) {
-                    drawPixelBoard(g2d,pList);
+                    drawPixelBoard(g2d, pList);
                     minIndex = j;
                     minLocation = pList.get(minIndex).getIndex();
                 }
@@ -411,8 +425,63 @@ public class Visualizer extends  JFrame {
         }
     }
 
-    public void bucketSort(ArrayList<pixelColor> whole, int numberOfBuckets, Graphics g2d, int delay) {
-        System.out.println(whole.size());
+    public void heapSort(ArrayList<pixelColor> whole, Graphics g2d, int delay) {
+        int n = whole.size();
+
+        for (int i = (n/2 -1); i >= 0; i--) {
+            heapify(whole, n, i, g2d, delay);
+        }
+
+        for (int i = n-1; i > 0; i--) {
+            pixelColor temp = whole.get(0);
+            whole.set(0, whole.get(i));
+            wait(delay);
+            drawPixelBoard(g2d, whole);
+            whole.set(i,temp);
+            wait(delay);
+            drawPixelBoard(g2d, whole);
+
+            heapify(whole, i, 0, g2d, delay);
+        }
+    }
+
+    public void heapify(ArrayList<pixelColor> whole, int n, int i, Graphics g2d, int delay) {
+        int largest = i;
+        int l = (2*i) + 1;
+        int r = (2*i) + 2;
+
+        if (l < n && whole.get(l).getIndex() > whole.get(largest).getIndex()) {
+            largest = l;
+        }
+
+        if (r < n && whole.get(r).getIndex() > whole.get(largest).getIndex()) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            pixelColor swap = whole.get(i);
+            whole.set(i,whole.get(largest));
+            drawPixelBoard(g2d, whole);
+            wait(delay);
+            whole.set(largest,swap);
+            drawPixelBoard(g2d, whole);
+            wait(delay);
+            heapify(whole,n,largest, g2d, delay);
+        }
+    }
+
+    public static pixelColor getMax(ArrayList<pixelColor> whole, int n) {
+
+        pixelColor mx = whole.get(0);
+
+        for(pixelColor p : whole) {
+            if (p.getIndex() > mx.getIndex()) mx = p;
+        }
+
+        return mx;
+    }
+
+    public void bucketSort(ArrayList<pixelColor> whole, int numberOfBuckets, Graphics g2d, int delay, int pixelSize) {
         ArrayList[] buckets = new ArrayList[numberOfBuckets];
         for (int i = 0; i < numberOfBuckets; i++) {
             buckets[i] = new ArrayList();
@@ -425,7 +494,7 @@ public class Visualizer extends  JFrame {
         int pixelsDrawn = 0;
         for(ArrayList<pixelColor> bucket : buckets) {
             bubbleSort(bucket,g2d,delay, pixelsDrawn);
-            pixelsDrawn+=bucket.size();
+            pixelsDrawn+=(bucket.size()*pixelSize);
         }
 
         int i = 0;
@@ -435,7 +504,7 @@ public class Visualizer extends  JFrame {
                 whole.set(i++,p);
             }
         }
-        drawPixelBoard(g2d, whole, i);
+        drawPixelBoard(g2d, whole, pixelsDrawn);
     }
 
     public int hash (int num, int numBuckets) {
@@ -471,38 +540,26 @@ public class Visualizer extends  JFrame {
         }
     }
 
-    void countingSort(ArrayList<pixelColor> whole, Graphics g2d, int delay) {
+    void gnomeSort(ArrayList<pixelColor> whole, Graphics g2d, int delay) {
+
         int n = whole.size();
 
-        ArrayList<pixelColor> output = new ArrayList<>(n);
+        int index = 0;
 
-
-        int[] count = new int[256];
-        for (int i = 0; i < 256; i++) {
-            count[i] = 0;
-        }
-
-        for(int i = 0; i < n; i++) {
-            ++count[whole.get(i).getIndex()];
-        }
-
-        for (int i = 1; i <= 255; i++) {
-            count[i] += count[i-1];
-        }
-
-        System.out.println(output.size());
-        System.out.println(n);
-        for (int i = n-1; i >= 0; i--) {
-            output.add((count[whole.get(i).getIndex()]-1), whole.get(i));
-            --count[whole.get(i).getIndex()];
-            drawPixelBoard(g2d,whole);
-            wait(delay);
-        }
-
-        for (int i = 0; i < n; i++) {
-            whole.set(i,output.get(i));
-            drawPixelBoard(g2d,whole);
-            wait(delay);
+        while (index < n) {
+            if (index == 0)
+                index++;
+            if (whole.get(index).getIndex() >= whole.get(index-1).getIndex())
+                index++;
+            else {
+                pixelColor temp;
+                temp = whole.get(index);
+                whole.set(index, whole.get(index-1));
+                whole.set(index-1, temp);
+                index--;
+                drawPixelBoard(g2d, whole);
+                wait(delay);
+            }
         }
     }
 }
